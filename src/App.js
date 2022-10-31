31b8a7d4595323bd0664c455581c00b5afbc898f
dml-ankit <ankit.aman@digimantra.com>
2022-10-31 12:03:45 +0530

    footer inprogress

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Routes from "./routes";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Footer from "./components/footer/Footer";
 
 function App() {
 
   return (
     <div className="App">
       <Routes />
+      <Footer />
     </div>
   );
 }
