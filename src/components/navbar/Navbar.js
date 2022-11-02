6c438644203bda3f7c179bfd2cc308711ae7beb7
manish kumar <manish.kumar@digimantra.com>
2022-11-02 16:03:19 +0530

    navbar ui

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,12 +6,17 @@ import profileIcon from "../../assets/images/homepage/profileIcon.png";
 import favouritesIcon from "../../assets/images/homepage/favouritesIcon.png";
 import cartIcon from "../../assets/images/homepage/cartIcon.png";
 import { FiChevronDown } from "react-icons/fi";
+import trendingImage1 from "../../assets/images/homepage/trendingImage1.png";
+import { CgClose } from "react-icons/cg";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
   return (
     <div className="nav-container">
       <div className="nav-upper">
         <img src={aloumNavLogo} alt="logo" className="nav-upper-logo" />
+        <input type="radio" name="slide" id="menu-btn"/>
+        <input type="radio" name="slide" id="cancel-btn"/>
         <div className="nav-upper-link-buttons">
           <div className="nav-searchbar">
             <img src={SearchIcon} alt="search here" />
@@ -23,14 +28,29 @@ const Navbar = () => {
             <img src={cartIcon} alt="cart" />
           </div>
         </div>
+        {/* hamburger btn */}
+        <label htmlFor="menu-btn" className="nav-btn nav-open-btn">
+          <HiOutlineMenuAlt3 />
+        </label>
       </div>
       {/* ************************************************************ */}
       <div className="nav-lower">
         <ul className="nav-lower-menu-links">
+          {/* mobile nav cancel btn */}
+          <label htmlFor="cancel-btn" className="nav-btn nav-close-btn">
+            <CgClose />
+          </label>
           <li>
             <div className="menu-name-and-arrow">
               HAIR <FiChevronDown />
             </div>
+            <input type="checkbox" id="showDropdown1" />
+            <label
+              htmlFor="showDropdown1"
+              className="mobile-menu-name-and-arrow"
+            >
+              HAIR <FiChevronDown />
+            </label>
             <ul className="dropDown-list">
               <li>Conditioner</li>
               <li>Hair oil</li>
@@ -42,6 +62,13 @@ const Navbar = () => {
             <div className="menu-name-and-arrow">
               FACE <FiChevronDown />
             </div>
+            <input type="checkbox" id="showDropdown2" />
+            <label
+              htmlFor="showDropdown2"
+              className="mobile-menu-name-and-arrow"
+            >
+              FACE <FiChevronDown />
+            </label>
             <ul className="dropDown-list">
               <li>Whitener</li>
               <li>Mask</li>
@@ -52,39 +79,60 @@ const Navbar = () => {
             <div className="menu-name-and-arrow">
               BODY <FiChevronDown />
             </div>
+            <input type="checkbox" id="showMobileMega" />
+            <label
+              htmlFor="showMobileMega"
+              className="mobile-menu-name-and-arrow"
+            >
+              BODY <FiChevronDown />
+            </label>
             <div className="mega-box">
-                <div className="mega-box-content">
-                    <div className="mega-box-row">
-                        <header>Heading 1</header>
-                        <ul className="mega-box-links">
-                            <li>Body oil</li>
-                            <li>Tanner</li>
-                            <li>Moisturiser</li>
-                        </ul>
-                    </div>
-                    <div className="mega-box-row">
-                        <header>Heading 2</header>
-                        <ul className="mega-box-links">
-                            <li>Spray</li>
-                            <li>Lotion</li>
-                            <li>Perfume</li>
-                        </ul>
-                    </div>
-                    <div className="mega-box-row">
-                        <header>Heading 3</header>
-                        <ul className="mega-box-links">
-                            <li>Cobblouise</li>
-                            <li>Yorlopi</li>
-                            <li>Smoothner</li>
-                        </ul>
-                    </div>
+              <div className="mega-box-content">
+                <div className="mega-box-row">
+                  <img
+                    src={trendingImage1}
+                    alt="menu dropdown view"
+                    className="megabox-row-image"
+                  />
                 </div>
+                <div className="mega-box-row">
+                  <header>Head</header>
+                  <ul className="mega-box-links">
+                    <li>Body oil</li>
+                    <li>Tanner</li>
+                    <li>Moisturiser</li>
+                  </ul>
+                </div>
+                <div className="mega-box-row">
+                  <header>Middle</header>
+                  <ul className="mega-box-links">
+                    <li>Spray</li>
+                    <li>Lotion</li>
+                    <li>Perfume</li>
+                  </ul>
+                </div>
+                <div className="mega-box-row">
+                  <header>Lower</header>
+                  <ul className="mega-box-links">
+                    <li>Cobblouise</li>
+                    <li>Yorlopi</li>
+                    <li>Smoothner</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </li>
           <li>
             <div className="menu-name-and-arrow">
               WELLNESS <FiChevronDown />
             </div>
+            <input type="checkbox" id="showDropdown3" />
+            <label
+              htmlFor="showDropdown3"
+              className="mobile-menu-name-and-arrow"
+            >
+              WELLNESS <FiChevronDown />
+            </label>
             <ul className="dropDown-list">
               <li>Grooming</li>
               <li>Hygiene</li>
@@ -95,6 +143,13 @@ const Navbar = () => {
             <div className="menu-name-and-arrow">
               INTIMATE <FiChevronDown />
             </div>
+            <input type="checkbox" id="showDropdown4" />
+            <label
+              htmlFor="showDropdown4"
+              className="mobile-menu-name-and-arrow"
+            >
+              INTIMATE <FiChevronDown />
+            </label>
             <ul className="dropDown-list">
               <li>Spa Oil</li>
               <li>Truffler</li>
