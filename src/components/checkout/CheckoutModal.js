a64d83b5a46dafbf2bf05ab0a0685c6b2f1a72cb
manish <themk85@gmail.com>
2022-11-08 20:10:57 +0530

    checkoutmodal

--- src/components/checkout/CheckoutModal.js
+++ src/components/checkout/CheckoutModal.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "./CheckoutModal.css";
 import { Dialog } from "@mui/material";
+import Gicon from "../../assets/icons/Gicon.png";
+import Ficon from "../../assets/icons/Ficon.png";
 import loginvector from "../../assets/images/cartpage/loginvector.jpg";
 import registervector from "../../assets/images/cartpage/registervector.jpg";
 import guestvector from "../../assets/images/cartpage/guestvector.jpg";
 
 const CheckoutModal = ({ openDialog, setOpenDialog }) => {
   return (
-    <Dialog open={openDialog} onClose={() => setOpenDialog(false)} maxWidth="lg">
+    <Dialog
+      open={openDialog}
+      onClose={() => setOpenDialog(false)}
+      maxWidth="lg"
+    >
       <div className="checkoutmodal-wrapper">
         <div className="checkoutmodal-signin">
           <div className="checkoutmodal-image-div">
@@ -15,11 +21,58 @@ const CheckoutModal = ({ openDialog, setOpenDialog }) => {
           </div>
           <div className="checkoutmodal-message">Have an Account?</div>
           <button className="checkoutmodal-primary-button">Sign In</button>
-          
+
+          <div className="checkoutModal-divider">
+            <div className="checkoutModal-left-div"></div>
+            <div className="checkoutModal-or">or</div>
+            <div className="checkoutModal-right-div"></div>
+          </div>
+          <div className="checkoutModal-other-Options">
+            <div className="checkout-google-logo-conatiner">
+              <img src={Gicon} alt="Google logo" />
+              <div className="checkoutModal-vai-google">Google</div>
+            </div>
+            <div className="checkout-facebook-logo-conatiner">
+              <img src={Ficon} alt="Facebook logo" />
+              <div className="checkoutModal-vai-facebook">Facebook</div>
+            </div>
+          </div>
         </div>
 
-        <div className="checkoutmodal-register">register</div>
-        <div className="checkoutmodal-guest">guest</div>
+        {/* ****************************** */}
+        <div className="checkoutmodal-register">
+          <div className="checkoutmodal-image-div">
+            <img src={registervector} alt="vector art" />
+          </div>
+          <div className="checkoutmodal-message">Don't have a Account?</div>
+          <button className="checkoutmodal-primary-button">Register</button>
+
+          <div className="checkoutModal-divider">
+            <div className="checkoutModal-left-div"></div>
+            <div className="checkoutModal-or">or</div>
+            <div className="checkoutModal-right-div"></div>
+          </div>
+          <div className="checkoutModal-other-Options">
+            <div className="checkout-google-logo-conatiner">
+              <img src={Gicon} alt="Google logo" />
+              <div className="checkoutModal-vai-google">Google</div>
+            </div>
+            <div className="checkout-facebook-logo-conatiner">
+              <img src={Ficon} alt="Facebook logo" />
+              <div className="checkoutModal-vai-facebook">Facebook</div>
+            </div>
+          </div>
+        </div>
+        {/* *********************************** */}
+        <div className="checkoutmodal-guest">
+          <div className="checkoutmodal-image-div">
+            <img src={registervector} alt="vector art" />
+          </div>
+          <div className="checkoutmodal-message">Checkout as Guest</div>
+          <button className="checkoutmodal-primary-button">
+            Continue as a guest
+          </button>
+        </div>
       </div>
     </Dialog>
   );
